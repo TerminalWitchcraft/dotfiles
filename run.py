d7e6ea6651f5a85772e5f1a658a1b12d9f6770d5
Hitesh Paul <hiteshpaul94@gmail.com>
2017-05-16 12:39:26 +0530

    Added code for system preparation

--- run.py
+++ run.py
@@ -7,4 +7,38 @@ from __future__ import absolute_import
 
 import os
 import sys
+import time
+import subprocess
 
+
+def run_cmd(cmd):
+    i = 0
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    print("Running {}".format(cmd))
+    os.system('setterm -cursor off')
+    while proc.poll() is None:
+        sys.stdout.write('\r')
+        if i  == 0:
+            sys.stdout.write('[/]')
+            i = 1
+        elif i == 1:
+            sys.stdout.write('[-]')
+            i = 2
+        else:
+            sys.stdout.write('[\\]')
+            i = 0
+        time.sleep(0.5)
+        sys.stdout.flush()
+    sys.stdout.write('\r')
+    print("Done...")
+    print(os.linesep)
+    os.system('setterm -cursor on')
+
+def main():
+    print("Preparing system.....")
+    run_cmd('sudo apt-get update')
+    run_cmd('sudo apt-get upgrade -y')
+
+
+if __name__ == "__main__":
+    main()
