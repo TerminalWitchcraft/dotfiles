d5d92a8e677ae13bc422abfb32767596eae19eba
Hitesh Paul <hiteshpaul94@gmail.com>
2017-05-17 18:43:27 +0530

    New files for vim

---
+++ editors/vim.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from ..utils.run import run_cmd
+
+
+class Vim(object):
+    """Class which sets up vim configuration"""
+    def __init__(self, platform, plugin_manager='Plug'):
+        self.platform = platform
+        self.plugin_manager = plugin_manager
+        if self.plugin_manager == 'Panthogen':
+            self.prefix = None
+        elif self.plugin_manager == 'Vundle':
+            self.prefix = 'Plugin'
+        else:
+            self.prefix = 'Plug'
+
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+    def setup(self, default_config=True):
+        pass
+        if default_config:
+            run_cmd(self.platform, "./vim.sh {}".format(self.plugin_manager))
+        else:
+            pass
---
+++ editors/vim.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+dir='../config'
+
+if [ $1 == "Plug" ]
+then
+	echo "Creating new vim directory"
+	ln -s $dir/vim ~/.vim
+	ln -s $dir/vimrc ~/.vimrc
+	echo "Installing Vim Plug"
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	# rm $dir/vim/vim
+	echo "Installing plugins"
+	vim -c PlugInstall -c PlugClean -c q -c q
+elif [ $1 == "Vundle"]; then
+	#for vundle
+	echo "Creating new vim directory"
+	ln -s $dir/vim ~/.vim
+	ln -s $dir/vimrc ~/.vimrc
+	echo "Installing Vim Vundle"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	echo "Installing Plugins"
+else
+	#for panthogen
+	echo "Creating new vim directory"
+	ln -s $dir/vim ~/.vim
+	ln -s $dir/vimrc ~/.vimrc
+	echo "Installing Vim Panthogen"
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	echo "Installing Plugins"
+fi
+
+echo "Creating vim swap, undo and backup directories"
+mkdir -p ~/.vim/undo_files
+mkdir -p ~/.vim/backup_files
+mkdir -p ~/.vim/swap_files
+echo " Done..."
