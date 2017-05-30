596128159d21dc3926bff8e92462e2b79141339a
Hitesh Paul <hiteshpaul94@gmail.com>
2017-05-30 18:24:04 +0530

    Added sublime and emacs

---
+++ editors/emacs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+from utils.run import run_cmd
+from utils.validate import validate_choice
+
+
+class Emacs(object):
+    """
+    Class which sets up emacs configuration
+    """
+    name = 'emacs'
+    pass
