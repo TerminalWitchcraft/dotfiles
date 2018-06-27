32a8a2b0bd19d983bdb5bb1eb2ff0bddc02df67d
Hitesh Paul <hiteshpaul94@gmail.com>
2018-06-27 17:10:54 +0530

    Added manjaro config

---
+++ manjaro/awesome/autorun.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ; then
+    $@&
+  fi
+}
+# run nm-applet
+run setxkbmap -option caps:swapescape
+run unclutter -idle 2
+run dbus-launch update-checker
+run light-locker
+run pulseaudio -D
+run redshift
+# run compton --shadow-exclude '!focused'
+# run xcape -e 'Super_L=Super_L|Shift_L|p'
+run /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
+# run thunar --daemon
+run xfce4-power-manager
+run pa-applet
+run xrdb merge ~/.Xresources
+run xfsettingsd
+run gnome-keyring-daemon
+run urxvtd
